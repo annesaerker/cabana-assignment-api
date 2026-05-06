2dc43d128d9a9e54a53613c5457a3fb0d5dbe660
Anne Saerker <32764898+annesaerker@users.noreply.github.com>
2026-05-06 19:45:13 +0200

    Refactor data import to use file system read

--- api/hero.js
+++ api/hero.js
@@ -1,8 +1,14 @@
-import data from './data/kommune-hero.json' assert { type: 'json' }
-
+import { readFileSync } from 'fs'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+ 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const data = JSON.parse(readFileSync(join(__dirname, 'data/kommune-hero.json'), 'utf8'))
+ 
 export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET')
   res.setHeader('Cache-Control', 's-maxage=3600')
   res.status(200).json(data)
 }
+ 
